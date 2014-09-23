600843fb6f14d415300ab80bcc4d716e5f4bcac1
Fleischers <Fleischer@meta.ua>
2014-09-23 20:31:00 +0300

    experss added

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello Gorlif Sense fan!\n');
-}).listen(process.env.PORT, process.env.IP);
+//var http = require('http');
+//http.createServer(function (req, res) {
+//    res.writeHead(200, {'Content-Type': 'text/plain'});
+//    res.end('Hello Gorlif Sense fan!\n');
+//}).listen(process.env.PORT, process.env.IP);
+var express = require('express')
+var app = express();
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
