fbea8e2fe0424c476481495aace1f5a3050ff75d
Fleischers <Fleischer@meta.ua>
2014-09-23 21:36:00 +0300

    Procfile

--- server.js
+++ server.js
@@ -6,11 +6,11 @@
 var express = require('express')
 var app = express();
 
-app.set('port', (process.env.PORT || 5050))
+app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('Hello Gorlif Sense fan!')
 })
 
 app.listen(app.get('port'), function() {
